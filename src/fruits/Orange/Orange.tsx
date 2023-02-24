4d63955bf4563fc7dda563a78256ef7403e62946
Trond Albinussen <trond.albinussen@blinq.no>
2023-02-24 22:54:10 +0100

    feat(pear and apple): added fruits

---
+++ src/fruits/Orange/Orange.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Orange() {
+  return <div>Orange</div>;
+}
---
+++ src/fruits/Pear/Pear.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Pear() {
+  return <div>Pear</div>;
+}
